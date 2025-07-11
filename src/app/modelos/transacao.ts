a80fcbb0270afdf982f339825675e8073d36111c
Luis Ismael <luis_ismael.mp@hotmail.com>
2025-07-11 17:05:00 -0300

    Mais alguns ajustes, quase lá!

--- src/app/modelos/transacao.ts
+++ src/app/modelos/transacao.ts
@@ -1,10 +1,15 @@
+import { nanoid } from "nanoid";
+
 export class Transacao {
+    readonly id = nanoid();
+    readonly data = new Date();
+
     constructor (
        public readonly tipo: TipoTransacao,
        public readonly valor: number
     ){}
 }
 export enum TipoTransacao{
-    DEPOSITO = 'deposito',
-    SAQUE = 'saque'
+    DEPOSITO = 'Depósito',
+    SAQUE = 'Saque'
 }
